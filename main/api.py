4216a8ff8750b19ef790f5ebe67584ce5a7b61a0
Ata <155850761+torn-exchange@users.noreply.github.com>
2025-01-15 02:52:41 +0100

    make API work

--- main/api.py
+++ main/api.py
@@ -3,7 +3,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
-from .models import Listing, Profile, Item  # Import necessary models
+from .models import Listing, Profile, Item
 
 from .profile_stats import return_profile_stats
 
@@ -15,19 +15,14 @@ so I will start of with a /api/test endpoint that will respond with json output 
 
 """
 
-### API FUNCTIONS ###
-
-
-### Actual API handling ###
-
 def api_home(request):
     return render(request, 'main/api_home.html')
+
 
 @csrf_exempt
 def test(request):
     if request.method == 'GET':
         try:
-
             result = f"API is working"
 
             return JsonResponse({"status": "success", "result": result})
@@ -36,26 +31,36 @@ def test(request):
     else:
         return JsonResponse({"status": "error", "message": "Invalid HTTP method"})
 
+
 @csrf_exempt
 def get_item_price(request):
-    # I was thinking tornexchange.com/api/price?user_id=1&item_id=1
-
-    if request.method == 'POST':
+    
+    if request.method == 'GET':
         try:
-            user_id = request.POST.get('user_id')
-            item_id = request.POST.get('item_id')
+            user_id = request.GET.get('user_id')
+            item_id = request.GET.get('item_id')
 
-            profile = get_object_or_404(Profile, id=user_id)
-            item = get_object_or_404(Item, id=item_id)
+            profile = get_object_or_404(Profile, torn_id=user_id)
+            item = get_object_or_404(Item, item_id=item_id)
 
             listing = Listing.objects.filter(owner=profile, item=item).first()
 
             if listing:
-                return JsonResponse({"status": "success", "price": listing.effective_price})
+                return JsonResponse({
+                    "status": "success",
+                    "data": {
+                        "trader": listing.owner.name,
+                        "price": listing.effective_price,
+                        "item": listing.item.name
+                    }
+                })
             else:
                 return JsonResponse({"status": "error", "message": "Listing not found"})
 
         except Exception as E:
-            return JsonResponse({"status": "error", "message": f"Invalid request parameters", "error": str(E)})
+            return JsonResponse({
+                "status": "error", 
+                "message": f"Invalid request parameters", "error": str(E)
+            })
     else:
         return JsonResponse({"status": "error", "message": "Invalid HTTP method"})
