b624d80347112be6c9108d204ab77d2696070b30
Ballard-dev <williamballard@outlook.com>
2025-03-09 11:57:54 +0000

    Added comments

--- main/static/main/js/view_utilities.js
+++ main/static/main/js/view_utilities.js
@@ -1,7 +1,10 @@
+// Toggle view layout
 const toggleViewLayout = function (container, target, layout = null) {
+  // Available layouts
   let layouts = ['grid-view', 'list-view'];
 
   let layoutPreferences = localStorage.getItem('view_layouts');
+  // Grab User layout preferences and parse the JSON string and convert it into a JavaScript object
   layoutPreferences = layoutPreferences ? JSON.parse(layoutPreferences) : {};
 
   if (layout === null) {
@@ -9,28 +12,35 @@ const toggleViewLayout = function (container, target, layout = null) {
   }
 
   if (!layouts.includes(layout)) {
+    // Default layout
     layout = 'grid-view';
   }
 
+  // Remove all layouts
   container.classList.remove(...layouts);
+  // Add the selected layout
   container.classList.add(layout);
 
   layoutPreferences[target] = layout;
+  // Save user preference layout
   localStorage.setItem('view_layouts', JSON.stringify(layoutPreferences));
 };
 
+// Set the view layout
 const setViewLayouts = function () {
   let layoutPreferences = localStorage.getItem('view_layouts');
   layoutPreferences = layoutPreferences ? JSON.parse(layoutPreferences) : {};
 
   for (let target in layoutPreferences) {
     let container = document.getElementById(target);
+    // Check if the container exists, so that we don't run unnecessary logic
     if (container) {
       toggleViewLayout(container, target, layoutPreferences[target]);
     }
   }
 }
 
+// Initialize the view layout
 const boot = function () {
   let viewTogglers = document.querySelectorAll('.view-toggler');
   if (viewTogglers.length > 0) {
@@ -38,10 +48,12 @@ const boot = function () {
 
     viewTogglers.forEach(function (viewToggler) {
       viewToggler.addEventListener('click', function (e) {
+        // Prevent the default action of the link, if any, recommended to use <a href="javascript:void(0)">
         e.preventDefault();
         let target = this.getAttribute('data-target');
         let layout = this.getAttribute('data-layout');
         let container = document.getElementById(target);
+        // Check if the container exists, so that we don't run unnecessary logic
         if (container) {
           toggleViewLayout(container, target, layout);
         }
@@ -51,5 +63,6 @@ const boot = function () {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
+  // Restores user preference layout on page load
   boot();
 });
