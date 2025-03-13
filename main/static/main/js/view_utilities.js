b48120b90f675e83d5531b8b4ef634d1204c0454
Ballard-dev <williamballard@outlook.com>
2025-03-13 10:01:51 +0000

    Updated view for more mobile accessibility

--- main/static/main/js/view_utilities.js
+++ main/static/main/js/view_utilities.js
@@ -40,6 +40,15 @@ const setViewLayouts = function () {
   }
 }
 
+function handleFilterResize() {
+    const element = document.querySelector('.filter-container');
+    if (window.innerWidth < 768) {
+        element?.classList.remove('filter-show');
+    } else {
+        element?.classList.add('filter-show');
+    }
+}
+
 // Initialize the view layout
 const boot = function () {
   let viewTogglers = document.querySelectorAll('.view-toggler');
@@ -56,6 +65,24 @@ const boot = function () {
         // Check if the container exists, so that we don't run unnecessary logic
         if (container) {
           toggleViewLayout(container, target, layout);
+        }
+      });
+    });
+  }
+
+  handleFilterResize();
+  let filterTogglers = document.querySelectorAll('.filter-toggler');
+  console.log(filterTogglers);
+  if (filterTogglers.length > 0) {
+    filterTogglers.forEach(function (filterToggler) {
+      console.log(filterToggler);
+      filterToggler.addEventListener('click', function (e) {
+        console.log('clicked');
+        e.preventDefault();
+        let target = this.getAttribute('data-target');
+        let container = document.getElementById(target);
+        if (container) {
+          container.classList.toggle('filter-show');
         }
       });
     });
