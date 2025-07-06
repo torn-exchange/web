a7bdc6b7a893d0a45f9893f63800369e816feb35
Hunter Jones <45572109+JonesTheHunter@users.noreply.github.com>
2025-07-06 16:18:36 -0400

    Update settings.py

--- torntrades/settings.py
+++ torntrades/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'main', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
